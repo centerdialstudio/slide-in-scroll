fb6672a7533ef143862fb1708f99267c7812a25a
Chris Anderson <chris.anderson@my.wheaton.edu>
2014-02-15 22:34:42 -0600

    Added ability to have multiple elements attached

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ var dom = require('dom'),
     scrolling = require('scrolling'),
     visible = require('visible');
 
-var init = function(element) {
-    $el = dom(element);
-    $el.each(function(el, i) {
-        if (visible(el[0], true)) {
-            el.addClass("already-visible"); 
-        }
-    });
-    var displayArticles = function() {    
-        $el.each(function(el, i) {
+var elements = [];
+
+var loop = function() { 
+    for (e in elements) {
+        elements[e].each(function(el, i) {
             if (visible(el[0], true)) {
                 if (!el.hasClass("come-in") && !el.hasClass("already-visible")) {
                     el.addClass("come-in"); 
@@ -18,8 +14,18 @@ var init = function(element) {
             } 
         });
     }
-    scrolling(displayArticles);
 }
 
+var init = function(element) {
+    $el = dom(element);
+    $el.each(function(el, i) {
+        if (visible(el[0], true)) {
+            el.addClass("already-visible"); 
+        }
+    });
+    elements.push($el);
+}
+
+scrolling(loop);
 
 module.exports = init;
